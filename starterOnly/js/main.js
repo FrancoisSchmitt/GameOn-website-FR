d308adc383c82a1256a2b9d4835a361ec1c59b19
François <Schmitt.francois.1699@gmail.com>
2022-02-11 21:19:20 +0100

    check radio button

--- starterOnly/js/main.js
+++ starterOnly/js/main.js
@@ -5,17 +5,12 @@ function editNav() {
     } else {
         x.className = "topnav";
     }
-}
+};
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
-const confirmModal = document.querySelector(".confirm-modal")
-const confirmClose = document.querySelector(".confirm-close");
-const confirmCloseBtn = document.querySelector(".confirm-modal-btn");
-const eventForm = document.querySelector("form");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,4 +1,34 @@
+// DOM
+const firstname = document.forms["reserve"]["first"];
+const lastname = document.forms["reserve"]["name"];
+const email = document.forms["reserve"]["email"];
+const birthdate = document.forms["reserve"]["birthdate"];
+const quantity = document.forms["reserve"]["quantity"];
+const locationInput = document.querySelectorAll(".checkbox-input[type=radio]");
+const checkboxInput = document.getElementById("checkbox1");
+const formData = document.querySelectorAll(".formData");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
 
+
+// DOM event form
+const eventForm = document.querySelector("form");
+
+// DOM form validate
+const confirmModal = document.querySelector(".confirm-modal")
+const confirmClose = document.querySelector(".confirm-close");
+const confirmCloseBtn = document.querySelector(".confirm-modal-btn");
+
+// date Format
+const dateCurrent = new Date();
+
+// regex
+const regexNumb = /^[0-9]+$/;
+const validRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 // Fermer le modal de confirmation.
 confirmClose.onclick = function () {
@@ -14,114 +44,105 @@ eventForm.addEventListener("submit", (e) => {
   validate();
 });
 
-// Function qui vérifie que le prénom n'est pas vide ou < 2 
-function firstValid() {
-  const firstname = document.forms["reserve"]["first"];
-  if (firstname.value == null || firstname.value < 2) {
-    formData[0].setAttribute("data-success-visible", false);
+function validate() {
+  // lastname verif
+
+  if (firstname.value.length >= 2) {
+    formData[0].setAttribute("data-success-visible", true);
+    formData[0].setAttribute("data-error-visible", false);
+
+  }
+  else {
     formData[0].setAttribute("data-error-visible", true);
     formData[0].setAttribute("data-error", `Veuillez entrer 2 caractères ou plus pour le champ du prénom.`);
-    return false;
+  }
+
+  // name verif
+
+  if (lastname.value.length >= 2) {
+    formData[1].setAttribute("data-success-visible", false);
+
   }
   else {
-    return true;
-  };
-}
-
-// Function qui vérifie que le nom de famille n'est pas vide ou < 2  
-function nameValid() {
-
-  const name = document.forms["reserve"]["name"];
-  if (name.value == null || name.value < 2) {
-    formData[1].setAttribute("data-success-visible", false);
     formData[1].setAttribute("data-error-visible", true);
     formData[1].setAttribute("data-error", `Veuillez entrer 2 caractères ou plus pour le champ du nom.`);
-    return false;
+  }
+
+  // email verif
+
+  if (email.value.match(validRegexEmail)) {
+    formData[2].setAttribute("data-success-visible", false);
+
   }
   else {
-    formData[1].setAttribute("data-success-visible", true);
-    formData[1].setAttribute("data-error-visible", false);
-    return true;
-  };
-}
-
-// Function qui vérifie que l'email est valide avec un regex.
-function emailValid() {
-  const email = document.forms["reserve"]["email"];
-  const validRegexEmail = /^\S+@\S+.\S+$/;
-  if (!email.value.match(validRegexEmail)) {
-    formData[2].setAttribute("data-success-visible", false);
     formData[2].setAttribute("data-error-visible", true);
     formData[2].setAttribute("data-error", `Votre email n'est pas valide. exemple : email@to.me`);
-    return false;
-  } else {
-    return true;
   }
-}
 
-// Function qui vérifie si une date est entré.
-function dateValid() {
-  const birthdate = document.forms["reserve"]["birthdate"];
-  const dateCurrent = new Date();
+  // birthdate verif
 
-  if (birthdate.value == "") {
+  if (!birthdate.value == "") {
     formData[3].setAttribute("data-success-visible", false);
+
+  }
+  else {
     formData[3].setAttribute("data-error-visible", true);
     formData[3].setAttribute("data-error", `Vous devez entrer votre date de naissance.`);
-    return false
   }
-  else {
-    return true
-  }
-}
 
-// Function qui vérifie / valide uniquement si la réponse est un chiffre.
-function numbValid() {
-  const quantity = document.forms["reserve"]["quantity"];
-  const regexNumb = /^[0-9]+$/;
-  if (!quantity.value.match(regexNumb)) {
+  // quantity verif
+
+  if (quantity.value.match(regexNumb)) {
     formData[4].setAttribute("data-success-visible", false);
-    formData[4].setAttribute("data-error-visible", true);
-    formData[4].setAttribute("data-error", `Veuillez entré un nombre entre 0 et 99.`);
-    return false;
+
   }
   else {
-    return true;
+    formData[4].setAttribute("data-error", `Veuillez entré un nombre entre 0 et 99.`);
+    formData[4].setAttribute("data-error-visible", true);
   }
-}
-// Function avec boucle for qui vérifie si une localisation à bien été choisi.
-function locationValid() {
-  const locationInput = document.querySelectorAll(".checkbox-input[type=radio]");
-  for (let radio of locationInput) {
-    if (radio.checked === true)
-      return true;
-  }
-  formData[5].setAttribute("data-success-visible", false);
-  formData[5].setAttribute("data-error-visible", true);
-  formData[5].setAttribute("data-error", `Vous devez choisir une option.`);
-  return false;
-}
 
-// Function qui vérifie si la checkbox est bien coché.
-function checkboxValid() {
-  const checkboxInput = document.getElementById("checkbox1");
-  if (!checkboxInput.checked) {
+  //  radiobutton verif
+  // for (let radio of locationInput) {
+  //   if (radio.checked === true) {
+  //     formData[5].setAttribute("data-success-visible", false);
+  //   }
+  //   else {
+  //     formData[5].setAttribute("data-error", `Veuillez séléctioner une ville.`);
+  //     formData[5].setAttribute("data-error-visible", true);
+  //   }
+  //   console.log(locationInput.value);
+  // }
+
+  if (location1.checked || location2.checked || location3.checked ||
+    location4.checked || location5.checked || location6.checked) {
+    formData[5].setAttribute("data-success-visible", false);
+  }
+  else {
+    formData[5].setAttribute("data-error", `Veuillez séléctioner une ville.`);
+    formData[5].setAttribute("data-error-visible", true);
+  }
+
+
+
+  // checkboxinput verif
+
+  if (checkboxInput.checked) {
     formData[6].setAttribute("data-success-visible", false);
+  }
+  else {
     formData[6].setAttribute("data-error-visible", true);
     formData[6].setAttribute("data-error", `Vous devez vérifier que vous acceptez les termes et conditions.`);
-    return false
   }
-  else {
-    return true;
-  }
-}
-// Crée une fonction Validate qui vérifie si xValid === true Sinon le formulaire return false.
-function validate() {
-  if (firstValid() && nameValid() && emailValid() && dateValid() && numbValid() && locationValid() && checkboxValid() === true) {
-    modalbg.style.display = "none"
+
+  if (lastname.value && firstname.value && birthdate.value && quantity.value && email.value &&
+    ((location1.checked) || (location2.checked) || (location3.checked) || (location4.checked) ||
+      (location5.checked) || (location6.checked)) && checkbox1.checked === true) {
+    modalbg.style.display = "none";
     confirmModal.style.display = "block";
   }
   else {
     return false;
   }
 };
+
+

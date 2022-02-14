dfee060872a50d127f223fabbf8701335ae5bc06
François <Schmitt.francois.1699@gmail.com>
2022-02-14 14:37:38 +0100

    final push

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -27,6 +27,7 @@ const confirmCloseBtn = document.querySelector(".confirm-modal-btn");
 const dateCurrent = new Date();
 
 // regex
+const regexName = /^[a-zA-Z ]+$/;
 const regexNumb = /^[0-9]+$/;
 const validRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -34,107 +35,108 @@ const validRegexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 confirmClose.onclick = function () {
   confirmModal.style.display = "none";
 }
-
 confirmCloseBtn.onclick = function () {
   confirmModal.style.display = "none";
 }
 
+// Event on submit Form 
 eventForm.addEventListener("submit", (e) => {
   e.preventDefault();
   validate();
 });
 
 function validate() {
-  // lastname verif
 
-  if (firstname.value.length >= 2) {
+  // lastname check
+
+  if (firstname.value.length >= 2 && firstname.value.match(regexName)) {
     formData[0].setAttribute("data-success-visible", true);
     formData[0].setAttribute("data-error-visible", false);
-
+    console.log(`Validate`);
   }
   else {
     formData[0].setAttribute("data-error-visible", true);
     formData[0].setAttribute("data-error", `Veuillez entrer 2 caractères ou plus pour le champ du prénom.`);
+    console.log(`Not Validate`);
   }
 
-  // name verif
+  // name check
 
-  if (lastname.value.length >= 2) {
-    formData[1].setAttribute("data-success-visible", false);
-
+  if (lastname.value.length >= 2 && lastname.value.match(regexName)) {
+    formData[1].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[1].setAttribute("data-error-visible", true);
     formData[1].setAttribute("data-error", `Veuillez entrer 2 caractères ou plus pour le champ du nom.`);
+    console.log(`Not Validate`);
   }
 
-  // email verif
+  // email check
 
   if (email.value.match(validRegexEmail)) {
-    formData[2].setAttribute("data-success-visible", false);
-
+    formData[2].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[2].setAttribute("data-error-visible", true);
     formData[2].setAttribute("data-error", `Votre email n'est pas valide. exemple : email@to.me`);
+    console.log(`Not Validate`);
+
   }
 
-  // birthdate verif
+  // birthdate check
 
   if (!birthdate.value == "") {
-    formData[3].setAttribute("data-success-visible", false);
-
+    formData[3].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[3].setAttribute("data-error-visible", true);
     formData[3].setAttribute("data-error", `Vous devez entrer votre date de naissance.`);
+    console.log(`Not Validate`);
   }
 
-  // quantity verif
+  // quantity check
 
   if (quantity.value.match(regexNumb)) {
-    formData[4].setAttribute("data-success-visible", false);
-
+    formData[4].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[4].setAttribute("data-error", `Veuillez entré un nombre entre 0 et 99.`);
     formData[4].setAttribute("data-error-visible", true);
+    console.log(`Not Validate`);
   }
 
-  //  radiobutton verif
-  // for (let radio of locationInput) {
-  //   if (radio.checked === true) {
-  //     formData[5].setAttribute("data-success-visible", false);
-  //   }
-  //   else {
-  //     formData[5].setAttribute("data-error", `Veuillez séléctioner une ville.`);
-  //     formData[5].setAttribute("data-error-visible", true);
-  //   }
-  //   console.log(locationInput.value);
-  // }
+  // Location check
 
   if (location1.checked || location2.checked || location3.checked ||
     location4.checked || location5.checked || location6.checked) {
-    formData[5].setAttribute("data-success-visible", false);
+    formData[5].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[5].setAttribute("data-error", `Veuillez séléctioner une ville.`);
     formData[5].setAttribute("data-error-visible", true);
+    console.log(`Not Validate`);
+
   }
 
-
-
-  // checkboxinput verif
+  // checkboxinput check
 
   if (checkboxInput.checked) {
-    formData[6].setAttribute("data-success-visible", false);
+    formData[6].setAttribute("data-error-visible", false);
+    console.log(`Validate`);
   }
   else {
     formData[6].setAttribute("data-error-visible", true);
     formData[6].setAttribute("data-error", `Vous devez vérifier que vous acceptez les termes et conditions.`);
+    console.log(`Not Validate`);
+
   }
 
-  if (lastname.value && firstname.value && birthdate.value && quantity.value && email.value &&
+  if (lastname.value && firstname.value && email.value && birthdate.value && quantity.value &&
     ((location1.checked) || (location2.checked) || (location3.checked) || (location4.checked) ||
       (location5.checked) || (location6.checked)) && checkbox1.checked === true) {
     modalbg.style.display = "none";
